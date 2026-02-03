4c4ed7c1e694a688a123b68e1865d0a680039af3
SOUICI Mohamed Amine <mohamed-amine.souici@fgei.ummto.dz>
2026-02-03 14:36:04 +0100

    added salle model

--- BackEnd/models/Salle.js
+++ BackEnd/models/Salle.js
@@ -4,3 +4,35 @@ const sequelize = require('../config/database');
 const Salle = sequelize.define('Salle', {
     id: {
         type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nom: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descrption: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    capacite: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    prix: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    adresse: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    proprietaireId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    tableName: 'salles',
+    timestamps: false
+});
+module.exports = Salle;
