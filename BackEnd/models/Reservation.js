afa104932443505f7eb2505c393b5370801fce3f
SOUICI Mohamed Amine <mohamed-amine.souici@fgei.ummto.dz>
2026-02-03 14:47:24 +0100

    added reservation model

---
+++ BackEnd/models/Reservation.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('config/database');
+
+const Reservation = sequelize.define('Reservation', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    date_debut: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    date_fin: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: 'reservations',
+    timestamps: false
+});
+module.exports = Reservation;
