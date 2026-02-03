567321d761f03b59a902aaaa567fb2978fecc0d4
SOUICI Mohamed Amine <mohamed-amine.souici@fgei.ummto.dz>
2026-02-03 17:15:37 +0100

    fixed jwt.js

--- BackEnd/utils/jwt.js
+++ BackEnd/utils/jwt.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const generateToken = (user) => {
-  return jwt.sign(
-    { id: user.id },
-    JWT_SECRET,
-    { expiresIn: "1d" }
-  );
+    return jwt.sign(
+        { id: user.id, role: user.role },
+        JWT_SECRET,
+        { expiresIn: "1d" }
+    );
 };
 
 module.exports = { generateToken, JWT_SECRET };
